a979ec5380927232030b4a7d4d87c2ac7235bab7
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-01-08 16:48:56 +0100

    Update insta.js

--- api/insta.js
+++ api/insta.js
@@ -1,10 +1,9 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 export default function handler(req, res) {
   const feed = 'https://rsshub.app/picuki/profile/filmkitnet'
   fetch(feed)
     .then(response => response.text())
-    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
     .then(data => {
       console.log(data);
       res.status(200).json({
