7a9c95477a5599d9fe4e089a34e41bebd1738812
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-01-08 17:08:36 +0100

    Update insta.js

--- api/insta.js
+++ api/insta.js
@@ -1,7 +1,18 @@
 const fetch = require('node-fetch');
+const { parse } = require('rss-to-json');
 
 export default function handler(req, res) {
   const feed = 'https://rsshub.app/picuki/profile/filmkitnet'
+  parse(feed).then(rss => {
+    console.log(JSON.stringify(rss, null, 3));
+    
+    res.status(200).json({
+    body: JSON.stringify(rss, null, 3)
+  });
+});
+  
+  
+  /*
   fetch(feed)
     .then(response => response.text())
     .then(data => {
@@ -9,5 +20,6 @@ export default function handler(req, res) {
       res.setHeader('Content-Type', 'text/xml');
       res.send(data)
     });
+    */
   
 }
